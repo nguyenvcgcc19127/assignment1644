618a11a824cdaded896b263ce4d1542d412dc24e
Nguyen Vo <nguyenvcgcc19127Gfpt.edu.vn>
2021-07-04 15:07:00 +0700

    Fixed heroku npm start

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //const url = "mongodb://localhost:27017/EmployeeDB";
-const url = "mongodb+srv://nguyenvo:0944778511@cluster0.8e9yf.mongodb.net/test"
+const url = "mongodb+srv://nguyenvo:0944778511@cluster0.8e9yf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
     else{
